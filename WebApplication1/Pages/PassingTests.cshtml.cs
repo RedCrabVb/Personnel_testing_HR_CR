fb2f2e8303a8b02e0e6fc2bdfc992aad881a316e
Александр <cany245@mail.ru>
2022-12-04 14:09:00 +0300

    Input id for gen test

--- WebApplication1/Pages/PassingTests.cshtml.cs
+++ WebApplication1/Pages/PassingTests.cshtml.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Personnel_testing_HR_CR.Data;
 using Personnel_testing_HR_CR.Data.Entity;
 
 namespace Personnel_testing_HR_CR.Pages
@@ -9,47 +11,38 @@ namespace Personnel_testing_HR_CR.Pages
         private readonly ILogger<PassingTestsModel> _logger;
         public string Name { get; set; }
         public TestEntity testEntity { get; set; }
+        public ApplicationDbContext _ctx;
 
-        public PassingTestsModel(ILogger<PassingTestsModel> logger)
+        public PassingTestsModel(ILogger<PassingTestsModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
-            testEntity = new TestEntity();
-            //var answer = new Answer();
-            //answer.Title = "Answer1";
-            //answer.Id = 1;
-            //var answer2 = new Answer();
-            //answer2.Title = "Answer2";
-            //answer2.Id = 2;
-
-            //var answer3 = new Answer();
-            //answer3.Title = "Answer3";
-            //answer3.Id = 1;
-            //var answer4 = new Answer();
-            //answer4.Title = "Answer4";
-            //answer4.Id = 2;
-
-            //var question = new Question();
-            //var answers = new List<Answer>();
-            //answers.Add(answer);
-            //answers.Add(answer2);
-
-            //var question2 = new Question();
-            //var answers2 = new List<Answer>();
-            //answers2.Add(answer3);
-            //answers2.Add(answer4);
-
-            //question.Answers = answers;
-            //question2.Answers = answers2;
-
-            //var questions = new List<Question>();
-            //questions.Add(question);
-            //questions.Add(question2);
-            //testEntity.Questions = questions;
+            _ctx = context;
         }
 
-        public void OnGet()
+        public void OnGet(int id)
         {
+            var test = _ctx.Tests.Where(x => x.Id == id).Include(x => x.Questions).First();
+
+            var questionsId = _ctx.Tests.Where(x => x.Id == id).Include(x => x.Questions).First();
+
+           var questions = _ctx.Questions.Where(q => questionsId.Questions.Contains(q)).Select(q => new Question
+            {
+                QuestionID = q.QuestionID,
+                QuestionText = q.QuestionText,
+                AnswerQ = q.AnswerQ,
+                Answers = q.Answers.Select(c => new Answer
+                {
+                    Id = c.Id,
+                    Title = c.Title
+                }).ToList()
+
+            }).ToList();
+
+            this.testEntity = test;
+            test.Questions = questions;
+
         }
+
 
         //[HttpPost]
         //[Route("Privacy/submit")]
