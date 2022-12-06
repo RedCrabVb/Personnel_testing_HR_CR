4a3959fca9b51c9fa94857d7830bfa2d069559f6
Александр <cany245@mail.ru>
2022-12-06 18:19:16 +0300

    Fix msg result test Add test all view

--- WebApplication1/Pages/TestAllView.cshtml.cs
+++ WebApplication1/Pages/TestAllView.cshtml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Personnel_testing_HR_CR.Data;
@@ -6,29 +6,28 @@ using Personnel_testing_HR_CR.Data.Entity;
 
 namespace Personnel_testing_HR_CR.Pages
 {
-    public class PassingTestsModel : PageModel
+    public class TestAllViewModel : PageModel
     {
+        public List<TestEntity> testEntitys { get; set; }
+        public TestEntity currentTest { get; set; }
         private readonly ILogger<PassingTestsModel> _logger;
-        public string Name { get; set; }
-        public TestEntity testEntity { get; set; }
         public ApplicationDbContext _ctx;
-        public bool isEnd = false;
-        public PassingTestsModel(ILogger<PassingTestsModel> logger, ApplicationDbContext context)
+
+        public TestAllViewModel(ILogger<PassingTestsModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
             _ctx = context;
         }
 
-        public void OnGet(int id)
-        {
-            if (id == -1)
-            {
-                this.isEnd = true;
-            }
-            else {
-                var test = _ctx.Tests.Where(x => x.Id == id).Include(x => x.Questions).First();
 
-                var questionsId = _ctx.Tests.Where(x => x.Id == id).Include(x => x.Questions).First();
+        public void OnGet()
+        {
+            testEntitys = new List<TestEntity>();
+            var tests = _ctx.Tests.Include(x => x.Questions).ToList();
+
+            foreach (var test in tests)
+            {
+                var questionsId = _ctx.Tests.Include(x => x.Questions).First();
 
                 var questions = _ctx.Questions.Where(q => questionsId.Questions.Contains(q)).Select(q => new Question
                 {
@@ -43,11 +42,18 @@ namespace Personnel_testing_HR_CR.Pages
 
                 }).ToList();
 
-                this.testEntity = test;
                 test.Questions = questions;
+
+                testEntitys.Add(test);
             }
+
+            this.testEntitys = testEntitys;
         }
 
-
+        public void OnPost(int id)
+        {
+            OnGet();
+            currentTest = testEntitys.Find(x => x.Id == id);
+        }
     }
 }
